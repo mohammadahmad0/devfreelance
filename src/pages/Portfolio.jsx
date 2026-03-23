6ae826e31b9437964c8f79ca8ca61f9608c4fd7b
mohammad ahmad <webdesignershub00@gmail.com>
2026-03-24 02:19:14 +0500

    Fix missing imports in Portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Helmet } from 'react-helmet-async';
 import ProjectCard from '../components/ProjectCard';
 
 // Project Images
